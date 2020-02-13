da43ac9332f5cb0320f786862ddae3a731b72631
My name is Alexander <18frsevastopol@gmail.com>
2020-02-14 00:28:05 +0200

    Implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
   Rails.application.routes.draw do
   get 'sessions/new'
-
   get 'users/new'
-
   root 'static_pages#home'
 
   get       '/help',    to: 'static_pages#help'
@@ -13,6 +11,6 @@
   post        '/login',   to: 'sessions#create'
   delete     '/logout',  to: 'sessions#destroy'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
 end
